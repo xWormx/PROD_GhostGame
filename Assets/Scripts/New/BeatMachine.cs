5037399b978fb42a2aeca55813b2bb68325ad798
Joel Larsson Wendt <156666927+joellarssonwendt@users.noreply.github.com>
2025-10-16 16:03:53 +0200

    BeatMachine + NotePlayer

---
+++ Assets/Scripts/New/BeatMachine.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BeatMachine : MonoBehaviour
+{
+    public static BeatMachine Instance;
+
+    private void Awake()
+    {
+        if (BeatMachine.Instance != null && BeatMachine.Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            BeatMachine.Instance = this;
+        }
+    }
+
+    public UnityEvent tick = new();
+
+    [SerializeField] private float bpm = 100.0f;
+    [SerializeField] private int notes = 8;
+    [SerializeField] private AudioClip kick, snare, hihat;
+
+    private AudioSource audioSource;
+    private int tickCounter = 0;
+    private int beatCounter = 0;
+    private double nextEventTime;
+    private bool running = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (!running)
+        {
+            return;
+        }
+
+        double time = AudioSettings.dspTime;
+
+        if (time + 1.0f > nextEventTime)
+        {
+            Tick();
+            nextEventTime += 60.0f / bpm / 4f;
+        }
+    }
+
+
+    void Tick()
+    {
+        tick.Invoke();
+
+        tickCounter++;
+        //Debug.Log($"Note: {tickCounter} / 8");
+
+        if (tickCounter % 2 == 1)
+        {
+            audioSource.PlayOneShot(hihat);
+        }
+
+        if (tickCounter == 1)
+        {
+            audioSource.PlayOneShot(kick);
+        }
+
+        if (beatCounter % 2 == 1 && tickCounter == 3)
+        {
+            audioSource.PlayOneShot(kick);
+        }
+
+        if (tickCounter == 5)
+        {
+            audioSource.PlayOneShot(snare);
+        }
+
+        if (tickCounter >= notes)
+        {
+            tickCounter = 0;
+            beatCounter++;
+            //Debug.Log($"Beat: {beatCounter}");
+        }
+    }
+
+    public void ChangeBPM(float f)
+    {
+        bpm += f;
+    }
+
+    public void Run(bool b)
+    {
+        if (b)
+        {
+            nextEventTime = AudioSettings.dspTime + 2.0f;
+        }
+
+        tickCounter = 0;
+        beatCounter = 0;
+        running = b;
+    }
+
+    public int GetTick()
+    {
+        return tickCounter;
+    }
+}
